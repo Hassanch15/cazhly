34cce288e7f6ebe45937c299bd5516bcdb624696
Usman Ali <usman.okara@hotmail.com>
2019-12-16 15:38:46 +0500

    heroku

--- users/users.controller.js
+++ users/users.controller.js
@@ -65,7 +65,8 @@ module.exports = router;
 }*/
 
 function register(req, res, next) {
-    fileIsValid(req, req.files.profile_image);
+    if (!req.files)
+        fileIsValid(req, req.files.profile_image);
     userService.create(req.body, req.files)
         .then(() => {
             res.status(200).json({"message": "account created"})
@@ -97,7 +98,8 @@ function getById(req, res, next) {
 }
 
 function update(req, res, next) {
-    fileIsValid(req, req.files.profile_image)
+    if (!req.files)
+        fileIsValid(req, req.files.profile_image)
     userService.update(req.body.id, req.body, req.files)
         .then(() => res.json({"message": "account updated"}))
         .catch(err => next(err));

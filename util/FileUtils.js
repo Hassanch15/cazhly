63cd11aead3d221edb16f374793124b0a92651f4
Usman Ali <usman.okara@hotmail.com>
2019-12-18 11:22:56 +0500

    product controoler

--- util/FileUtils.js
+++ util/FileUtils.js
@@ -6,7 +6,7 @@ function fileIsValid(reqFile) {
 
 function multiplefileIsValid(req, reqFiles) {
     try {
-        req.files.forEach((file) => {
+        reqFiles.forEach((file) => {
             if (file.mimetype != "image/jpeg" && file.mimetype != "image/png") {
                 throw "Only PNG/JPEG is supported";
             }

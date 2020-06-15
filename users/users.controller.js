34262feb19b593bd9ceb62deb6650d5067a5997f
hassanch756 <hassan.ch.756@gmail.com>
2020-06-15 12:50:01 +0500

    initial commit

--- users/users.controller.js
+++ users/users.controller.js
@@ -37,7 +37,7 @@ function register(req, res, next)
 
     userService.create(req.body, req.files)
         .then((user) => {
-            res.status(200).json({"message": "account created",user})
+            res.status(201).json({"message": "account created",user})
         })
         .catch( err => {
             if (validateField(req.body.uid)) {
